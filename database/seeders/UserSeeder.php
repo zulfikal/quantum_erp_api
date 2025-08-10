5965686ce21e4b5203ff5f11241643355c286a0f
zulfikal <mzulfikal99@gmail.com>
2025-08-10 17:10:57 +0800

    add Admin Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@quantum.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
